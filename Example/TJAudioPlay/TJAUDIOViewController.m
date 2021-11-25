9aa7dc3704653b868323e84d2e919f31abf25131
SuperSun <>
2021-11-25 16:00:24 +0800

    no message

--- Example/TJAudioPlay/TJAUDIOViewController.m
+++ Example/TJAudioPlay/TJAUDIOViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "TJAUDIOViewController.h"
+#import "TJAudioPlayManager.h"
+#import "TJAudioPlayViewManger.h"
 
 @interface TJAUDIOViewController ()
 
@@ -17,7 +19,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
+    UIButton *actionBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    actionBtn.backgroundColor = [UIColor orangeColor];
+    [actionBtn setTitle:@"播放" forState:UIControlStateNormal];
+    actionBtn.frame = CGRectMake(0, 0, 100, 100);
+    actionBtn.center = self.view.center;
+    [actionBtn addTarget:self action:@selector(actionClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:actionBtn];
+    
+}
+
+-(void)actionClick{
+    
+    //加载资源
+    TJMediaBackGroundModel *model = [[TJMediaBackGroundModel alloc]init];
+    model.mediaId = @"1";
+    model.coverUrl = @"https://tianjiutest.oss-cn-beijing.aliyuncs.com/tojoy/tojoyClould/backstageSystem/image/1631168736433.jpg";
+    model.auther = @"AudioPlayDemo";
+    model.mediaUrl = @"https://tianjiutest.oss-cn-beijing.aliyuncs.com/tojoy/tojoyClould/serverUpload/202109/01/image/1630459636944.mp3";
+    
+    
+    //展示UI
+    if(true){
+        [TJAudioPlayViewManger audioSourceData:@[model]];
+        [TJAudioPlayViewManger show];
+        [TJAudioPlayViewManger playWithModel:[TJAudioPlayViewManger getAudioSourceData].firstObject];
+    }else{
+        
+        //    不展示UI
+        [TJAudioPlayManager openBackGround:YES];
+        [TJAudioPlayManager audioSourceData:@[model]];
+        [TJAudioPlayManager playWithModel:model];
+    }
 }
 
 - (void)didReceiveMemoryWarning
--- TJAudioPlay.podspec
+++ TJAudioPlay.podspec
@@ -29,14 +29,12 @@ TODO: Add long description of the pod here.
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '9.0'
-
-  s.source_files = 'TJAudioPlay/Classes/**/*'
   
-  # s.resource_bundles = {
-  #   'TJAudioPlay' => ['TJAudioPlay/Assets/*.png']
-  # }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.public_header_files = 'Pod/Classes/**/*.h'
+  s.source_files = 'TJAudioPlay/Classes/**/*'
+  s.resource_bundles = {
+     'TJAudioPlay' => ['TJAudioPlay/Assets/*.png']
+  }
+  s.dependency 'SDWebImage'
+  s.dependency 'StreamingKit'
 end
